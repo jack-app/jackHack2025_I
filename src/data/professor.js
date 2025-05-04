bdd9e44a4571fb1d30fdf233ae49ac751855c70e
Kumafuji1202 <bassclefottavaalta@gmail.com>
2025-05-04 13:40:56 +0900

    add placeholder for professor image urls in prof data file

--- src/data/professor.js
+++ src/data/professor.js
@@ -3,6 +3,7 @@ const professors = [
     id: 1,
     name: "雨日 アロハ",
     personality: "おおらかで天気と気分に左右される自由人",
+    image: "../assets/professor/test.png",
     pieces: [],
     endings: {
       friend: [
@@ -91,6 +92,7 @@ const professors = [
     id: 2,
     name: "鍵山 忍",
     personality: "冷静沈着なスパルタ型、努力を重視する完璧主義者",
+    image: "../assets/professor/test.png",
     pieces: [],
     endings: {
       friend: [
@@ -186,6 +188,7 @@ const professors = [
     id: 3,
     name: "速杉 瞬",
     personality: "超天才型、全てを一瞬で見抜く冷静な理論派",
+    image: "../assets/professor/test.png",
     pieces: [],
     endings: {
       friend: [
@@ -279,6 +282,7 @@ const professors = [
     id: 4,
     name: "出席 透",
     personality: "一見優しいが、油断すると一気に追い込んでくる飄々型",
+    image: "../assets/professor/test.png",
     pieces: [],
     endings: {
       friend: [
