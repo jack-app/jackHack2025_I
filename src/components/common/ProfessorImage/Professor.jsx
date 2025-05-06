a87bbc2191fa11b8545bcde131cb05e58af4e286
er1ru <erieru.310@outlook.jp>
2025-05-06 13:19:56 +0900

    professor add

--- src/components/common/ProfessorImage/Professor.jsx
+++ src/components/common/ProfessorImage/Professor.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import "../../../styles/components/common/professor-image.css"
 import ProfessorAmabi from "../../../assets/professor/Amabi.png"
-// import ProfessorKagiyama from "../../../assets/professor/Kagiyama.png"
-const ProfessorImages = [ProfessorAmabi]
+import ProfessorKagiyama from "../../../assets/professor/2.png"
+import ProfessorHayasugi from "../../../assets/professor/3.png"
+import ProfessorSyusseki from "../../../assets/professor/4.png"
+const ProfessorImages = [ProfessorAmabi,ProfessorKagiyama,ProfessorHayasugi,ProfessorSyusseki];
 
 const Professor = ({professorId}) => {
     return (
